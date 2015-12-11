8bee61eef14893307b16d99ac26672e4076cab0c
Ivan Atanasov <iv.atanasow@gmail.com>
2015-12-11 10:44:44 +0200

    Adding week3 problems

--- Week3/src/friday/Students.java
+++ Week3/src/friday/Students.java
@@ -1,54 +1,53 @@
 package friday;
 
 public class Students implements Comparable<Students> {
-	
+
 	private String name;
 	private int grade;
-	
-	
-	public Students(String _name , int _grade) {
+
+	public Students(String _name, int _grade) {
 		this.name = _name;
 		this.grade = _grade;
 	}
-	
+
 	public int getGrade() {
 		return grade;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
+
 	@Override
-	public int hashCode(){		
+	public int hashCode() {
 		return name.hashCode();
 	}
-	
+
 	@Override
-	public boolean equals(Object obj){
-		if(obj == null){
+	public boolean equals(Object obj) {
+		if (obj == null) {
 			return false;
 		}
-		if(!this.getClass().equals(obj.getClass())){
+		if (!this.getClass().equals(obj.getClass())) {
 			return false;
 		}
 		Students s = (Students) obj;
-		
+
 		return (this.name.equals(s.name) && this.grade == s.grade);
 	}
 
 	@Override
 	public int compareTo(Students o) {
-		if(this.grade == o.grade){
+		if (this.grade == o.grade) {
 			return this.name.compareTo(o.name);
-		}
-		else{
-			if(this.grade < o.grade){
+		} else {
+			if (this.grade < o.grade) {
 				return -1;
-			}
-			else{
+			} else {
 				return 1;
 			}
 		}
